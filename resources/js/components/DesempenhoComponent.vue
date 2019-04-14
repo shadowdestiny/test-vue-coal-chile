00ced1ebdec92f5df823b75c43b15f310134f231
lmarin <lrm.programador@gmail.com>
2019-04-14 12:31:37 -0400

    delete class container

--- resources/js/components/DesempenhoComponent.vue
+++ resources/js/components/DesempenhoComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div>
         <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
             <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
             <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
